152555967ed8a6abdb73b47d966946822671a1be
fleon-psi <filip-karol.leonarski@psi.ch>
2020-06-12 19:43:15 +0200

    Added zoom function to Preview
    Signed-off-by: fleon-psi <filip-karol.leonarski@psi.ch>

--- frontend_ui/src/components/Preview.js
+++ frontend_ui/src/components/Preview.js
@@ -1,12 +1,12 @@
-
 import React, {Component} from 'react';
 import Slider from '@material-ui/core/Slider';
-import Grid from '@material-ui/core/Grid';
+import PinchZoomPan from "react-responsive-pinch-zoom-pan";
 
 class Preview extends Component {
   state = {
      contrast: 10.0,
-     image: 0
+     image: 0,
+     logarithmic: false
   }
 
   componentDidMount() {
@@ -26,10 +26,14 @@ class Preview extends Component {
   };
 
   render() {
-     return <div>
-        <Grid container spacing={2}><Grid item xs={12}>
-        <img src={"http://mx-jungfrau-1:5232/preview/" + this.state.contrast}/></Grid></Grid> 
-        <Slider defaultValue={10.0} min={1.0} max={200} onChange={this.sliderMoved}></Slider></div>;
+     return <div style={{ width: '80%', margin: 'auto' }}>
+        <div style={{ height: '1000px', margin: 'auto'}}>        
+        <PinchZoomPan maxScale={4.0}>
+        <img src={"http://mx-jungfrau-1:5232/" + (this.state.logarthmic?"preview_log/":"preview/") + this.state.contrast} alt='Preview'/> 
+        </PinchZoomPan>
+        </div>
+        <Slider defaultValue={10.0} min={1.0} max={200} onChange={this.sliderMoved}></Slider>
+        </div>;
   }
 }
 
