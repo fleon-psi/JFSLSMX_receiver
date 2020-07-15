c2370a13b198bb829155feddbe90ee60130a6347
fleon-psi <filip-karol.leonarski@psi.ch>
2020-07-15 11:19:51 +0200

    Improved geometry based on Kabsch 2009 paper

--- include/xray.h
+++ include/xray.h
@@ -19,6 +19,16 @@
 
 #include "JFApp.h"
 
+inline float dot_product(float x[3], float y[3]) {
+    return x[0]*y[0] + x[1]*y[1] + x[2]*y[2];
+}
+
+inline void cross_product(float x[3], float y[3], float z[3]) {
+    z[0] = x[1]*y[2]-x[2]*y[1];
+    z[1] = x[0]*y[2]-x[2]*y[0];
+    z[2] = x[0]*y[1]-x[1]*y[0];
+}
+
 // Lab is in mm
 // TODO: Ensure signs are OK
 inline void detector_to_lab(float x, float y, float lab[3]) {
@@ -33,17 +43,17 @@ inline void detector_to_lab(float x, float y, float lab[3]) {
 
 // For the time being, code below assumes "standard" diffraction geometry
 // While capturing most of the cases, this will also optimize execution time
-// Assume S0 = {0, 0, 1/lambda}, d1 = {1, 0, 0}, d2 = {0, 1, 0}, d3 = {0, 0, 1}
+// Assume |S0|^2 = 1/lambda, d1 = {1, 0, 0}, d2 = {0, 1, 0}, d3 = {0, 0, 1}
 // see Kabsch, Acta Cryst D66, 133-144
 // one_over_wavelength is expressed in A^-1
-inline void lab_to_reciprocal(float p[3], float lab[3], float one_over_wavelength) {
+inline void lab_to_reciprocal(float p[3], float lab[3], float one_over_wavelength, float S0[3]) {
     // [mm^-1] - this terms makes lab / one_over_norm_factor dimensionless and ensures norm == 1
     float one_over_norm_factor = 1.0/sqrt(lab[0]*lab[0] + lab[1]*lab[1] + lab[2]*lab[2]);
 //    float one_over_wavelength = experiment_settings.energy_in_keV / 12.398; [A^-1]
 
-    p[0] = lab[0] * one_over_wavelength * one_over_norm_factor;
-    p[1] = lab[1] * one_over_wavelength * one_over_norm_factor;
-    p[2] = (lab[2] * one_over_norm_factor - 1) * one_over_wavelength ;
+    p[0] = lab[0] * one_over_wavelength * one_over_norm_factor - S0[0];
+    p[1] = lab[1] * one_over_wavelength * one_over_norm_factor - S0[1];
+    p[2] = lab[2] * one_over_wavelength * one_over_norm_factor - S0[2];
 }
 
 // Assume m2 = {0, 1, 0}
@@ -52,6 +62,16 @@ inline void reciprocal_rotate(float p0[3], float p[3], float omega_in_radian) {
     p0[0] = p[0];
     p0[1] = cos(omega_in_radian) * p[1] - sin(omega_in_radian) * p[2];
     p0[2] = sin(omega_in_radian) * p[1] + cos(omega_in_radian) * p[2];
+}
+
+// The function moves p back into origin of rotation
+inline void reciprocal_rotate(float p0[3], float p[3], float m1[3], float m2[3], float m3[3], float omega_in_radian) {
+    float p_m1 = dot_product(m1,p) * cos(omega_in_radian) - dot_product(m3,p) * sin(omega_in_radian);
+    float p_m2 = dot_product(m2,p);
+    float p_m3 = dot_product(m3,p) * cos(omega_in_radian) + dot_product(m1,p) * sin(omega_in_radian);
+    p0[0] = p_m1 * m1[0] + p_m2 * m2[0] + p_m3 * m3[0];
+    p0[1] = p_m1 * m1[1] + p_m2 * m2[1] + p_m3 * m3[1];
+    p0[2] = p_m1 * m1[2] + p_m2 * m2[2] + p_m3 * m3[2];
 }
 
 inline float get_resolution(float lab[3]) {
